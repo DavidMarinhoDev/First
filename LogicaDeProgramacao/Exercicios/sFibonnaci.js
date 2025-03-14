5aa9004ec52a55e0308d20f3f6eff535e89049e9
David Marinho <loinedavid34@gmail.com>
2025-03-13 23:32:06 -0300

    Create sFibonnaci.js

---
+++ LogicaDeProgramacao/Exercicios/sFibonnaci.js
@@ -0,0 +1,14 @@
+var t1 = 0;
+console.log(t1)
+var t2 = 1;
+console.log(t2)
+var t3 = 0;
+
+let c = 0;
+for (c = 1; c <= 13; ){
+    t3 = t1 + t2
+    console.log(t3)
+    t1 += t2
+    t2 += t3
+}
+
