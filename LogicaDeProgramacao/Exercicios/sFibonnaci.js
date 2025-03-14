b0b4cf9161436f3f369597b1effa4c73fe0be2ab
David Marinho <loinedavid34@gmail.com>
2025-03-13 23:41:28 -0300

    Update sFibonnaci.js

--- LogicaDeProgramacao/Exercicios/sFibonnaci.js
+++ LogicaDeProgramacao/Exercicios/sFibonnaci.js
@@ -4,11 +4,16 @@ var t2 = 1;
 console.log(t2)
 var t3 = 0;
 
-let c = 0;
-for (c = 1; c <= 13; c++){
+
+function proximoFibonacci (){
     t3 = t1 + t2
     console.log(t3)
     t1 += t2
     t2 += t3
 }
 
+var c = 0;
+for (c += 3; c <= 10; c++){
+    proximoFibonacci(t1,t2)
+}
+ 
