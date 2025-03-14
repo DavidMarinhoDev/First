4aa0e6f59dce5c2a8e867fa87a55dae17e9edf85
David Marinho <loinedavid34@gmail.com>
2025-03-14 00:19:56 -0300

    add

--- LogicaDeProgramacao/Exercicios/function.js
+++ LogicaDeProgramacao/Exercicios/function.js
@@ -1,17 +1,11 @@
+const prompt = require('prompt-sync')({sigint:true})
 
-
-var soma = 0;
+var s = 0;
 var x = 0;
 var y = 0;
 
-function somar() {
-    x = x + 1;
-    y = y + 2;
-    soma = x + y 
-    console.log(`O valor de X: ${x}`);
-    console.log(`O valor de Y: ${y}`);
-    console.log(`A Soma de ${x} + ${y} : ${soma}`);
- 
+function somar(a,b){
+    return a + b 
 }
 
 x = parseInt(prompt('Digite um numero: '))
@@ -20,10 +14,12 @@ y = parseInt(prompt('Digite outro numero: '))
 if(isNaN(x) || isNaN(y)){
     console.log('Digite um numero valido!')
 } else {
-    somar(x , y);
+    s = somar(x , y);
+    console.log(`O valor de X: ${x}`);
+    console.log(`O valor de Y: ${y}`);
+    console.log(`A Soma de ${x} + ${y} : ${s}`);
 }
 
-console.log(`Valor de X = ${x}`);
-console.log(`Valor de Y = ${y}`);
+
 
 
