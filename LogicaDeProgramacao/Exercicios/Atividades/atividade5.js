20779b9b4a7a4824f1a4b6cf12856d6887c26ce6
David Marinho <loinedavid34@gmail.com>
2025-03-26 16:37:14 -0300

    Create atividade5.js

---
+++ LogicaDeProgramacao/Exercicios/Atividades/atividade5.js
@@ -0,0 +1,27 @@
+// Matricula: 25114290113 
+// Nome: David Marinho Gonçalves Fonseca
+// Atividade 05
+
+const prompt = require ('prompt-sync') ({sigint : true});
+
+console.log("Escolha uma cor : [VERDE], [AMARELO], [VERMELHO]")
+var per = prompt(": ");
+var res = per.toLowerCase();
+
+if ( res !== "verde" && res !== "amarelo" && res !== "vermelho"){
+    console.log("Escolha uma cor válida!");
+} else {
+    switch (res){
+        case "verde" :
+            console.log("Siga!");
+            break;
+        case "amarelo" :
+            console.log("Atenção!");
+            break;
+        case "vermelho" : 
+            console.log("Pare!")
+            break;
+        default :
+            break;
+    }
+}
