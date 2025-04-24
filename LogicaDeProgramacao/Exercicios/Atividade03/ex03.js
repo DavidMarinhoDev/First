30ae4f04e3bfcff9645cbde086f4e211b1ff3e59
David Marinho <loinedavid34@gmail.com>
2025-04-24 19:41:14 -0300

    sla

--- LogicaDeProgramacao/Exercicios/Atividade03/ex03.js
+++ LogicaDeProgramacao/Exercicios/Atividade03/ex03.js
@@ -3,19 +3,15 @@
 
 const prompt = require('prompt-sync')({sigint: true});
 
-let numeroPremiado1 = 11;
-let numeroPremiado2 = 22;
-let numeroPremiado3 = 33;
-let palpite = parseInt(prompt('Escolha um número premiado, você tem 4 tentativas: '));
-var acerto = false;
+let numeroPremiado1 = parseInt(Math.random() * 100);
+let numeroPremiado2 = parseInt(Math.random() * 100);
+let numeroPremiado3 = parseInt(Math.random() * 100);
 
-for (let tentativas = 0; tentativas < 3; tentativas++){
+for (let tentativas = 1; tentativas <= 3; tentativas++){
+    let palpite = parseInt(prompt(`Escolha um número premiado, tentativa numero ${tentativas}: `));
     if (palpite === numeroPremiado1 || palpite === numeroPremiado2 || palpite === numeroPremiado3) {
         console.log(`Parabéns! Você acertou! O número premiado é: ${palpite}`);
-        acerto += true;
         break;
-    }else if( acerto == false){
-        palpite = parseInt(prompt('Digite o outro número até acertar: '));
     }
 }
 console.log(`Os números premiados foram: ${numeroPremiado1}, ${numeroPremiado2}, ${numeroPremiado3}`);
---
+++ LogicaDeProgramacao/SalaDeAula/tags.js
@@ -0,0 +1,24 @@
+let num1 = 5.29;
+let num2 = -5.29;
+
+let chao1 = Math.floor(num1);
+let chao2 = Math.floor(num2);
+let teto1 = Math.ceil(num1);
+let teto2 = Math.ceil(num2);
+
+console.log(chao1);
+console.log(chao2);
+console.log(teto1);
+console.log(teto2);
+
+let num3 = 7.13;
+let num4 = 4.90;
+
+let arredon3 = Math.round(num3);
+let arredon4 = Math.round(num4);
+
+console.log(arredon3,arredon4);
+console.log(Math.round(7.5));
+console.log(Math.pow(10,Math.pow(2,2.5)));
+
+console.log(Math.random().toFixed(2))
